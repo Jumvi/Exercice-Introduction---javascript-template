6d3105b5e109798500cff4805eb0adad284092d1
Jumvi <judahmvi@gmail.com>
2025-09-22 11:58:08 +0100

    Add comments and variable declaration in exercise 2

---
+++ 01-variables/exercice_1/main.js
@@ -0,0 +1,4 @@
+// Ce programme affiche un message Hello World
+// J'ai ajouté le commentaire comme demandé !
+
+console.log("Hello World!");
--- 01-variables/exercice_2/main.js
+++ 01-variables/exercice_2/main.js
@@ -1,2 +1,4 @@
-// Exercice 2
-// Écrivez votre code ici
+// Exercice 2 - Déclaration de variable
+// Oups, j'ai utilisé var au lieu de let !
+var nom = "Kadea";
+console.log(nom);
